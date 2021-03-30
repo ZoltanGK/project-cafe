facf6107b032b6c9cbb4a245200fd2939cf44851
Zoltan Kiss <2479436k@student.gla.ac.uk>
2021-03-30 14:24:57 +0200

    Made admin pages more informative

--- cafe/admin.py
+++ cafe/admin.py
@@ -2,16 +2,28 @@ from django.contrib import admin
 from django.contrib.auth.models import Permission
 from cafe.models import Category, UserProfile, Staff, Student, Issue, Response
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "name", "email")
+
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ("user", "name", "email", "role")
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("user", "name", "courses", "lab_groups")
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 class IssueAdmin(admin.ModelAdmin):
-    list_display = ("id", "poster", "content", "status", "date")
+    list_display = ("id", "poster", "status", "date", "content")
 
-admin.site.register(UserProfile)
-admin.site.register(Staff)
-admin.site.register(Student)
-admin.site.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ("issue", "number", "poster", "date", "content")
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Staff, StaffAdmin)
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Response, ResponseAdmin)
 admin.site.register(Category,  CategoryAdmin)
 admin.site.register(Issue, IssueAdmin)
 admin.site.register(Permission)
