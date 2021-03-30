8116f6f923a7c74cc31f4bd6874018c540aeedfe
Zoltan Kiss <2479436k@student.gla.ac.uk>
2021-03-30 13:58:06 +0200

    Registered new models on admin site for testing

--- cafe/admin.py
+++ cafe/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
+from cafe.models import Category, UserProfile, Staff, Student, Issue, Response
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ("id", "poster", "content", "status", "date")
+
+admin.site.register(UserProfile)
+admin.site.register(Staff)
+admin.site.register(Student)
+admin.site.register(Response)
+admin.site.register(Category,  CategoryAdmin)
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Permission)
