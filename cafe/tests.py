ec06d85873856383e9ff9c7d58bf8278ff4c1217
Zoltan Kiss <2479436k@student.gla.ac.uk>
2021-04-06 14:10:31 +0200

    Updated tests for unassigned users and split tests into subtests

--- cafe/tests.py
+++ cafe/tests.py
@@ -27,7 +27,6 @@ class BaseTestCase(TestCase):
         for cat in Category.objects.all():
             permission = Permission.objects.get(codename = f"resp-for-{cat.slug}")
             admin_user.user_permissions.add(permission)
-        
 
 class ViewAndTemplateTests(BaseTestCase):
     def setUp(self):
@@ -39,26 +38,36 @@ class ViewAndTemplateTests(BaseTestCase):
         super().setUp()
         self.client.login(username='testAdmin', password='@dmin123')
         self.response_dict = {}
-
-    def test_views_exist(self):
         for url in VIEW_NAMES:
             try:
                 self.response_dict[url] = self.client.get(reverse(url))
             except NoReverseMatch:
-                self.fail(f"VIEW NOT FOUND: {url} defined in spec couldn't be matched to a view.")
+                self.response_dict[url] = "FAIL"
+
+    def test_views_exist(self):
+        for url, response in self.response_dict.items(): 
+            with self.subTest():
+                if response == "FAIL":
+                    self.fail(f"VIEW NOT FOUND: {url} defined in spec couldn't be matched to a view.")
 
 
     def test_urls_work(self):
         for url, response in self.response_dict.items():
-            self.assertEquals(response.status_code, 200, f"PAGE NOT FOUND: {url} defined in spec couldn't be succesfully accessed by superuser.")
+            with self.subTest():
+                if not (response.status_code == 200 or response.status_code == 302):   
+                    self.assertEquals(response.status_code, 200, f"PAGE NOT FOUND: {url} defined in spec couldn't be succesfully accessed by superuser.")
     
     def test_pages_link_to_contact_us(self):
         for url, response in self.response_dict.items():
-            self.assertTrue(reverse("contact") in response.content.decode(), f"NO CONTACT US LINK:  {url} contained no link to the Contact Us page for superuser.")
+            with self.subTest():
+                if response.status_code != 302:
+                    self.assertTrue("Contact Us" in response.content.decode(), f"NO CONTACT US LINK:  {url} contained no link to the Contact Us page for superuser.")
     
     def test_pages_allow_logout(self):
         for url, response in self.response_dict.items():
-            self.assertTrue("Log Out" in response.content.decode(), f"NO LOG OUT LINK: {url} contained no 'Log Out' option for superuser.")
+            with self.subTest():
+                if response.status_code != 302:
+                    self.assertTrue("Logout" in response.content.decode(), f"NO LOG OUT LINK: {url} contained no 'Logout' option for superuser.")
 
     def test_no_undefined_pages(self):
         self.assertTrue(len(VIEW_NAMES) == len(cafe.urls.urlpatterns), f"NON-SPEC VIEW_NAMES FOUND: Found URLs defined in urls.py that were not mentioned in the design spec.")
@@ -80,8 +89,10 @@ class StaffViewTests(BaseTestCase):
 
     def test_staff_sees_correct_issues(self):
         response_login = self.client.get(reverse("staff_account"))
-        self.assertTrue("General Tutor Feedback" in response_login.content.decode(), f"CATEGORY NOT FOUND: user 'jsmith' couldn't see the 'General Tutor Feedback' category in '{reverse('staff_account')}'.")
-        self.assertTrue("Test Category" in response_login.content.decode(), f"CATEGORY NOT FOUND: user 'jsmith' couldn't see the 'Test Category' category in '{reverse('staff_account')}'.")       
+        with self.subTest():
+            self.assertTrue("General Tutor Feedback" in response_login.content.decode(), f"CATEGORY NOT FOUND: user 'jsmith' couldn't see the 'General Tutor Feedback' category in '{reverse('staff_account')}'.")
+        with self.subTest():
+            self.assertTrue("Test Category" in response_login.content.decode(), f"CATEGORY NOT FOUND: user 'jsmith' couldn't see the 'Test Category' category in '{reverse('staff_account')}'.")       
 
 class StudentViewTests(BaseTestCase):
     def setUp(self):
@@ -92,11 +103,37 @@ class StudentViewTests(BaseTestCase):
     def test_student_has_correct_access(self):
         response_login = self.client.get(reverse("student_account"))
         response_view_queries = self.client.get(reverse("view_queries"))
-        self.assertEqual(response_login.status_code, 200, f"STAFF DENIED ACCESS: Student couldn't access their login page ({reverse('student_account')}).")
-        self.assertEqual(response_view_queries.status_code, 200, f"STAFF DENIED ACCESS: Student couldn't access their query viewing page ({reverse('view_queries')}).")
+        with self.subTest():
+            self.assertEqual(response_login.status_code, 200, f"STAFF DENIED ACCESS: Student couldn't access their login page ({reverse('student_account')}).")
+        with self.subTest():
+            self.assertEqual(response_view_queries.status_code, 200, f"STAFF DENIED ACCESS: Student couldn't access their query viewing page ({reverse('view_queries')}).")
 
     def test_student_sees_correct_issues(self):
         response_view_queries = self.client.get(reverse("view_queries"))
-        self.assertTrue("Anonymous More Cooper" in response_view_queries.content.decode(), f"ISSUE NOT FOUND: user 'ay' couldn't see the 'Anonymous More Cooper' issue in '{reverse('view_queries')}'.")
-        self.assertTrue("Named More Cooper" in response_view_queries.content.decode(), f"ISSUE NOT FOUND: user 'ay' couldn't see the 'Named More Cooper' issue in '{reverse('view_queries')}'.")
-        self.assertFalse("Test Issue 1" in response_view_queries.content.decode(), f"WRONG ISSUE FOUND: user 'ay' saw 'Test Issue 1' issue in '{reverse('view_queries')}', even though they shouldn't have access.")
+        with self.subTest():
+            self.assertTrue("Anonymous More Cooper" in response_view_queries.content.decode(), f"ISSUE NOT FOUND: user 'ay' couldn't see the 'Anonymous More Cooper' issue in '{reverse('view_queries')}'.")
+        with self.subTest():
+            self.assertTrue("Named More Cooper" in response_view_queries.content.decode(), f"ISSUE NOT FOUND: user 'ay' couldn't see the 'Named More Cooper' issue in '{reverse('view_queries')}'.")
+        with self.subTest():
+            self.assertFalse("Test Issue 1" in response_view_queries.content.decode(), f"WRONG ISSUE FOUND: user 'ay' saw 'Test Issue 1' issue in '{reverse('view_queries')}', even though they shouldn't have access.")
+
+class UnassignedViewTests(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.client.logout()
+        User.objects.create_user('testUser', 'user@user.com', '@dmin123')
+        self.client.login(username='testUser', password='@dmin123')
+        self.response_dict = {}
+        for url in VIEW_NAMES:
+            try:
+                self.response_dict[url] = self.client.get(reverse(url))
+            except NoReverseMatch:
+                self.response_dict[url] = "FAIL"
+    
+    def test_unassigned_redirects(self):
+        for url, response in self.response_dict.items():
+            with self.subTest():
+                if not (url == "login" or url == "logout_screen" or url == "register"
+                        or url == "wait" or url == "index" or url == "contact" or
+                        response.status_code == 302):
+                    self.fail(f"UNASSIGNED USER ACCESS: Unassigned user was not redirected from {url} to an appropriate page.")
