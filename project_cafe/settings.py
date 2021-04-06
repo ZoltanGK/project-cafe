ba90116e58381566a451dadfbb4ccfd9aaf356e0
Robbie <robbiet111@hotmail.co.uk>
2021-04-06 16:19:48 +0100

    Set Debug to False

--- project_cafe/settings.py
+++ project_cafe/settings.py
@@ -25,7 +25,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'f18hwojq(nxv#3%^d)8e_72x8$2sfzk417-f7tu0joj#c*wizc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['robbiet111.pythonanywhere.com']
 
